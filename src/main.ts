e5c10b0560db1cca6481f32c6bf8333c240667c3
CedricNoyel <cedric.noyel@gmail.com>
2021-08-20 00:02:22 +0200

    feat: improve security with helmet

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,6 @@
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
+import * as helmet from 'helmet';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
@@ -13,6 +14,8 @@ async function bootstrap() {
   const document = SwaggerModule.createDocument(app, config);
   SwaggerModule.setup('api', app, document);
 
+  app.use(helmet());
   await app.listen(3000);
 }
+
 bootstrap();
